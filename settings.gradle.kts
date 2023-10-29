a0f82b1cf85890f91787ee91d06dfded65f01ba8
Sebastian <sebasqbi@gmail.com>
2023-10-29 19:50:39 +0100

    Add build respositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "AndroidAppKotlin"
 include(":app")
