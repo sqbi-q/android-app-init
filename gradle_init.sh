88ef47f3427ce267f50af3b474787fe19ab46e23
Sebastian <sebasqbi@gmail.com>
2023-10-29 20:47:39 +0100

    Add project structure description

--- gradle_init.sh
+++ gradle_init.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 projectname="android_app_kotlin"
 
+## Result project structure ##
+# root/                      # Project
+#   |---gradle.properties
+#   |---build.gradle.kts
+#   |---settings.gradle.kts
+#   \---app/                 # Module
+#        |---build.gradle.kts
+#        |---build/
+#        |---libs/
+#        \---src/
+#              \---main/     # Source set
+#                   |---AndroidManifest.xml
+#                   |---kotlin/
+#                   |       |---com/example/myapp
+#                   |
+#                   |---res/
+#                        |---drawable/
+#                        |---values/
+#                        \---...
+#     
+# Mainly inspired by: 
+# https://developer.okta.com/blog/2018/08/10/basic-android-without-an-ide
+# and https://developer.android.com/build
+##
+
+
 # Gradle initalization (using .kts configs)
 gradle init --type basic --dsl kotlin --project-name $projectname --no-incubating
 
@@ -10,3 +36,12 @@ cp gradle_templates/build.gradle.kts ./build.gradle.kts
 cp gradle_templates/settings.gradle.kts ./settings.gradle.kts
 # Change rootProject.name to projectname 
 sed -i "s/rootProject.name \=.*/rootProject.name \= \"$projectname\"/" ./settings.gradle.kts
+
+
+# App (module) directory initalization
+mkdir app
+mkdir app/build/
+mkdir app/libs/
+mkdir -p app/src/main/kotlin/
+mkdir -p app/src/main/res/drawable
+mkdir -p app/src/main/res/values
