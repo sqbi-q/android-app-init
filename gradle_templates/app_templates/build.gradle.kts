68cde8df096c7a092d231ea3f7325a33c5fc50cb
Sebastian <sebasqbi@gmail.com>
2023-11-06 19:57:45 +0100

    Shape configurations on Empty Activity

--- gradle_templates/app_templates/build.gradle.kts
+++ gradle_templates/app_templates/build.gradle.kts
@@ -6,12 +6,13 @@ plugins {
 }
 
 dependencies {
-    implementation("androidx.compose.material3:material3:1.1.2")
-    implementation("androidx.compose.compiler:compiler:1.5.3")
-    implementation("androidx.compose.ui:ui:1.5.4")
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
-    implementation("androidx.compose.runtime:runtime:1.5.4")
-
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3:1.1.2")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 }
